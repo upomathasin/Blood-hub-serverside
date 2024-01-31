9997186bafe845783bbcb2235e18f489f44dddc1
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-01-31 15:28:19 +0600

    vercel config2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,22 +8,22 @@ require("dotenv").config();
 app.get("/", (req, res) => {
   console.log("hi !server is running!!");
 });
-const { MongoClient } = require("mongodb");
-const uri = process.env.MONGO_PASS;
+// const { MongoClient } = require("mongodb");
+// const uri = process.env.MONGO_PASS;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    //  await client.connect();
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //   await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     //  await client.connect();
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     //   await client.close();
+//   }
+// }
+// run().catch(console.dir);
 app.listen(5000, () => {
   console.log("listening on port ");
 });
