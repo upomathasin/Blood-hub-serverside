608598a670ebf711bd013a9bb350178d8780b104
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-01-31 15:53:52 +0600

    vercel config3

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ app.use(express.json());
 require("dotenv").config();
 
 app.get("/", (req, res) => {
-  console.log("hi !server is running!!");
+  res.send("hi !server is running!!");
 });
-// const { MongoClient } = require("mongodb");
-// const uri = process.env.MONGO_PASS;
+const { MongoClient } = require("mongodb");
+const uri = process.env.SECRET_MONGOURI;
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     //  await client.connect();
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     //   await client.close();
-//   }
-// }
-// run().catch(console.dir);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //  await client.connect();
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 app.listen(5000, () => {
-  console.log("listening on port ");
+  console.log("listening on port ", 5000);
 });
